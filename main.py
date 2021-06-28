606625150cd9301359c8a1d581a14d97e672df3d
javim24 <javi.mosconi@gmail.com>
2021-06-28 11:39:26 -0300

    Emprolijé código

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-"""
-    Cosas para hacer:
-        *crear funcion para simular la noche de apuestas  --> HECHO
-        *implementar un loop para simular cada apuesta de la noche --> HECHO
-        *simular cada apuesta con un valor aleatorio        --> HECHO
-        *terminar el loop si se cumplen 300 juegos o se queda sin fichas   --> HECHO
-        *devolver cantidad de fichas  y número de juegos        --> HECHO
-        *hacer un loop para correr la función 20 veces          --> HECHO   
-        *promediar los resultados           --> HECHO
-        *medir el tiempo que tarda en simularse cada noche e imprimirlo por pantalla
-"""
 import random
 import time
 
@@ -27,25 +16,33 @@ def noche_de_apuestas(fichas, prob_ganar, max_juegos):
                 Num juegos -> almacena la cantidad de juegos que se hicieron en la noche
     """
 
-    contador_juegos = 0
+    contador_juegos = 0                         #cuenta la cantidad de veces que se jugó
     while fichas > 0 and contador_juegos < max_juegos:
-        resultado = random.choices([0,1], weights=[1-prob_ganar, prob_ganar])
+        resultado = random.choices([0,1], weights=[1-prob_ganar, prob_ganar])   #resultado de la apuesta
+       
         if resultado[0]:
             fichas += 1
         else:
             fichas -= 1
+
         #print(f"Apuesta N°: {contador_juegos + 1}, tengo {fichas} fichas")
         contador_juegos += 1
-    return {"Num fichas" : fichas, "Num juegos" : contador_juegos}
 
+    return {"Num fichas" : fichas, "Num juegos" : contador_juegos}  #devuelve diccionario con resultados
+
+
+
+#comienzo de programa principal
 
 media_apuestas = 0
+
 for i in range(20):
-    t_start = time.time()
-    resultado =  noche_de_apuestas(50, 0.4, 300)
+    t_start = time.time()               #para medir el tiempo que tarda en ejecutarse
+    resultado =  noche_de_apuestas(50, 0.4, 300)        #llamada a la función para simular la noche de apuestas
     t_end = time.time()
     print(f"La simulación tardó {t_end - t_start} segundos")
     media_apuestas += resultado["Num juegos"]
     print(resultado)
+
 media_apuestas /= 20
 print(f"La media de la cantidad de apuestas es: {media_apuestas}")
