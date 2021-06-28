cd5040720a769f3cfd304564065164f593ed8e5e
javim24 <javi.mosconi@gmail.com>
2021-06-28 10:42:13 -0300

    Agrego valores de retorno de la función

--- main.py
+++ main.py
@@ -12,14 +12,21 @@ def noche_de_apuestas(fichas, probabilidad, max_juegos):
             max_juegos -> número máximo de juegos en una noche
         
         Salidas:
-            ....
+            Diccionario con claves:
+                Num fichas -> almacena la cantidad de fichas que tiene al final de la noche
+                Num juegos -> almacena la cantidad de juegos que se hicieron en la noche
     """
     contador_juegos = 0
     while fichas > 0 and contador_juegos < max_juegos:
-        
-        print(f"Apostando {contador_juegos} {fichas}")
-        fichas -= 1
+        resultado = 1   #acá iría la parte de random
+        if resultado:
+            fichas += 1
+        else:
+            fichas -= 1
+        print(f"Apuesta N°: {contador_juegos}, tengo {fichas} fichas")
         contador_juegos += 1
+    return {"Num fichas" : fichas, "Num juegos" : contador_juegos}
 
 
-noche_de_apuestas(50, 0.4, 300)
+resultado =  noche_de_apuestas(50, 0.4, 300)
+print(resultado)
